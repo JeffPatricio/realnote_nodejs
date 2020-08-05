b9dc7913e3c9178638af3018a1297091625dab6c
Jefferson Patrício <jeffersonpatricio1@gmail.com>
2020-08-05 09:48:52 -0300

    rota de status

--- src/server.js
+++ src/server.js
@@ -5,6 +5,9 @@ require('dotenv/config');
 const app = express();
 
 app.use('/', express.static(path.resolve('public')));
+
+app.get('/status', (req, res) => res.send('ok'));
+
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server running in port ${process.env.PORT}`);
 });
